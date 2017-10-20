e9fa94da94f7bcc9bb1217bb551a4bea8b8f345e
Robert Bryan <rbryan21@gmail.com>
2017-10-19 23:32:14 -0400

    Adding navbar component within a new module called shared, containing components shared throughout the site

---
+++ WebAdventure/WebAdventureClient/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from "./navbar/navbar.component";
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  exports: [
+    NavbarComponent
+  ]
+})
+export class SharedModule { }
