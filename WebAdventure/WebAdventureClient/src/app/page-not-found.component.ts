2172284e320528429af0567f9190c89745613391
Robert <rbryan21@gmail.com>
2017-10-20 11:33:53 -0400

    Finishing the homepage task. Modified the styles a bit, think it looks nice

--- WebAdventure/WebAdventureClient/src/app/page-not-found.component.ts
+++ WebAdventure/WebAdventureClient/src/app/page-not-found.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     template: `
-    <h1>This is not the page you were looking for!</h1>
+    
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Page Not Found!</h1>
+            <p class="lead">This is not the page you are looking for!</p>
+        </div>
+    </div>
+
+
     `
 })
 export class PageNotFoundComponent { }
