26fc4e3a9ea8656da7e8fa731606839bac33c8df
Robert <rbryan21@gmail.com>
2017-10-23 09:55:19 -0400

    Styling progressing nicely with the game info component. Got the basic html html form up, next comes the angularizing of the form and the service setup.

--- WebAdventure/WebAdventureClient/src/app/create/info/create-info.component.ts
+++ WebAdventure/WebAdventureClient/src/app/create/info/create-info.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../interfaces/game.interface';
 
 @Component({
   selector: 'info',
   templateUrl: './create-info.component.html',
   styleUrls: ['./create-info.component.scss']
 })
-export class CreateInfoComponent { }
+export class CreateInfoComponent implements OnInit {
+
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+
+}
