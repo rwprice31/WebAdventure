6d551d5e29a5ba7288f2ad8d612836b65ede40dc
Robert Bryan <rbryan21@gmail.com>
2017-11-07 18:01:17 -0500

    Adding click event handlers to the game card component.

--- WebAdventure/WebAdventureClient/src/app/my-games/edit/edit.component.ts
+++ WebAdventure/WebAdventureClient/src/app/my-games/edit/edit.component.ts
@@ -4,4 +4,10 @@ import { Component } from '@angular/core';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss']
 })
-export class EditComponent { }
+export class EditComponent {
+
+  private gameId: number;
+
+  
+  
+ }
