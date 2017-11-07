a9b539b5e4ca1b40625ac388ddad48f7c2f1b98f
kingoruga <koruga@ggc.edu>
2017-11-07 17:58:36 -0500

    bug fix attempt for reset form

--- WebAdventure/WebAdventureClient/src/app/resetpassword/resetpassword.component.ts
+++ WebAdventure/WebAdventureClient/src/app/resetpassword/resetpassword.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject} from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -10,6 +10,8 @@ import { matchOtherValidator } from "../shared/functions/match-other-validator";
 import { IUserResetPassword } from "../shared/interfaces/user-resetPassword.interface";
 import { IUserResetPasswordViewModel } from "../shared/interfaces/view-models/IUser-resetpassword-view-model";
 import { IUserResetPasswordResponse } from "../shared/interfaces/responses/reset-password-response";
+import { IToastr } from './../shared/interfaces/external-libraries/toastr.interface';
+import { TOASTR_TOKEN } from "../core/services/external-libraries/toastr.service";
 
 @Component({
     templateUrl: './resetpassword.component.html',
@@ -17,14 +19,14 @@ import { IUserResetPasswordResponse } from "../shared/interfaces/responses/reset
   })
 
 export class ResetPasswordComponent implements OnInit{
-  model = new resetpasswordModel('', '', '');
 
     resetForm: FormGroup;
 
     constructor(private http: Http,
       private router: Router,
       private formBuilder: FormBuilder,
-      private userService: UserService
+      private userService: UserService,
+      @Inject(TOASTR_TOKEN) private toastr: IToastr
     ) {
 
     }
@@ -49,8 +51,10 @@ export class ResetPasswordComponent implements OnInit{
         this.userService.resetPassword(user).subscribe((res: IUserResetPasswordResponse) => {
           if (res.status) {
             console.log('Status = true, res = ' + res);
+            this.toastr.success('Password Reset Successfull!');
           } else {
             console.log('Status = false, res = ' + res);
+            this.toastr.error('Password Reset Failed!');
           }
         });
       }
