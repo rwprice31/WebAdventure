22ecdff6693c1c12af685fe3116e4415b52dee87
Blake Norman <blakenorman37@gmail.com>
2017-11-24 22:34:51 -0500

    Client implementation start

---
+++ WebAdventure/WebAdventureClient/src/app/browse-games/browse-games-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BrowseGamesComponent } from './browse-games.component';
+import { PlayGameComponent } from './play/play.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: BrowseGamesComponent,
+        children: [
+            {
+                path: 'play/:id',
+                loadChildren: 'app/browse-games/play/play.module#PlayModule'
+            }
+        ]
+    }
+]
+
+@NgModule({
+    imports: [
+      RouterModule.forChild(routes)
+    ],
+    providers: [ ],
+    exports: [ RouterModule ]
+})
+
+export class BrowseGamesRoutingModule {
+    static components = [ PlayGameComponent ];
+}
